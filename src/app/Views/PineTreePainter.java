5edd20580598b3fea2e17b8444462a1f9398d36c
DESKTOP-5OQE9G3\Bart Renders <bart>
2017-06-13 01:08:40 +0200

    -

--- src/app/Views/PineTreePainter.java
+++ src/app/Views/PineTreePainter.java
@@ -15,58 +15,33 @@ public class PineTreePainter extends TreePainter {
     }
 
     public void paintTree(GraphicsContext gc) {
-        calcSize();
+        double xpos = calcX(getTree());
 
-        double xpos = calcX();
-        double ypos = calcY();
+        if(xpos < (getTree().getTreeSize().getLeafSize() / 2) || xpos - (getTree().getTreeSize().getLeafSize() * 2) > getSCREEN_WIDTH()){
+            paintTree(getTree(), gc);
+        } else {
+            double ypos = calcY(getTree());
 
-        gc.setFill(getTREE_TRUNK());
-        gc.fillRect(xpos, ypos, trunkWidth, totalHeight);
-        gc.strokeRect(xpos, ypos, trunkWidth, totalHeight);
+            gc.setFill(getTREE_TRUNK());
+            gc.fillRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
+            gc.strokeRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
 
-        gc.setFill(getTree().getColor());
-        double[] doubleX = {
-            xpos - totalHeight / 2 / 1.9 - totalHeight / 2 / 1.9,
-            xpos + totalHeight / 2 + trunkWidth * 1.5,
-            xpos + trunkWidth / 2
-        };
+            gc.setFill(getTree().getColor());
 
-        double[] doubleY = {
-            ypos - (totalHeight - leafSize) / 100 + 20,
-            ypos - (totalHeight - leafSize) / 100 + 20,
-            ypos - (totalHeight - leafSize) - totalHeight + 20
-        };
+            double[] doubleX = {
+                    xpos - getTree().getTreeSize().getLeafSize() / 2 - getTree().getTreeSize().getTrunkWidth() / 2,
+                    xpos + getTree().getTreeSize().getLeafSize() / 2 + getTree().getTreeSize().getTrunkWidth() * 1.5,
+                    xpos + getTree().getTreeSize().getTrunkWidth() / 2
+            };
 
-        gc.fillPolygon(doubleX, doubleY, 3);
-        gc.strokePolygon(doubleX, doubleY, 3);
+            double[] doubleY = {
+                    ypos - getTree().getTreeSize().getTrunkHeight() / 100 + 20,
+                    ypos - getTree().getTreeSize().getTrunkHeight() / 100 + 20,
+                    ypos - getTree().getTreeSize().getTrunkHeight() - getTree().getTreeSize().getLeafSize() + 20
+            };
 
-//        double xpos = calcX(getTree());
-//
-//        if(xpos < (getTree().getTreeSize().getLeafSize() / 2) || xpos - (getTree().getTreeSize().getLeafSize() * 2) > getSCREEN_WIDTH()){
-//            paintTree(getTree(), gc);
-//        } else {
-//            double ypos = calcY(getTree());
-//
-//            gc.setFill(getTREE_TRUNK());
-//            gc.fillRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-//            gc.strokeRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-//
-//            gc.setFill(getTree().getColor());
-//
-//            double[] doubleX = {
-//                    xpos - getTree().getTreeSize().getLeafSize() / 2 - getTree().getTreeSize().getTrunkWidth() / 2,
-//                    xpos + getTree().getTreeSize().getLeafSize() / 2 + getTree().getTreeSize().getTrunkWidth() * 1.5,
-//                    xpos + getTree().getTreeSize().getTrunkWidth() / 2
-//            };
-//
-//            double[] doubleY = {
-//                    ypos - getTree().getTreeSize().getTrunkHeight() / 100 + 20,
-//                    ypos - getTree().getTreeSize().getTrunkHeight() / 100 + 20,
-//                    ypos - getTree().getTreeSize().getTrunkHeight() - getTree().getTreeSize().getLeafSize() + 20
-//            };
-//
-//            gc.fillPolygon(doubleX, doubleY, 3);
-//            gc.strokePolygon(doubleX, doubleY, 3);
-//        }
+            gc.fillPolygon(doubleX, doubleY, 3);
+            gc.strokePolygon(doubleX, doubleY, 3);
+        }
     }
 }
