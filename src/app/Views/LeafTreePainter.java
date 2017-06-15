2d29991eb6a713f20df8c811f4477c1d2fcab177
DESKTOP-5OQE9G3\Bart Renders <bart>
2017-06-15 14:12:37 +0200

    -

--- src/app/Views/LeafTreePainter.java
+++ src/app/Views/LeafTreePainter.java
@@ -13,20 +13,6 @@ public class LeafTreePainter extends TreePainter {
     }
 
     public void paintTree(GraphicsContext gc) {
-        double xpos = calcX(getTree());
 
-        if (xpos < (getTree().getTreeSize().getLeafSize() / 2.0) || xpos - (getTree().getTreeSize().getLeafSize() * 2.0) > getSCREEN_WIDTH()) {
-            paintTree(getTree(), gc);
-        } else {
-            double ypos = calcY(getTree());
-
-            gc.setFill(getTREE_TRUNK());
-            gc.fillRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2.0), ypos - 5.0, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-            gc.strokeRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2.0), ypos - 5.0, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-
-            gc.setFill(getTree().getColor());
-            gc.fillOval(xpos - (getTree().getTreeSize().getLeafSize() / 2.0), ypos - getTree().getTreeSize().getLeafSize(), getTree().getTreeSize().getLeafSize(), getTree().getTreeSize().getLeafSize());
-            gc.strokeOval(xpos - (getTree().getTreeSize().getLeafSize() / 2.0), ypos - getTree().getTreeSize().getLeafSize(), getTree().getTreeSize().getLeafSize(), getTree().getTreeSize().getLeafSize());
-        }
     }
 }
--- src/app/Views/PineTreePainter.java
+++ src/app/Views/PineTreePainter.java
@@ -14,33 +14,6 @@ public class PineTreePainter extends TreePainter {
     }
 
     public void paintTree(GraphicsContext gc) {
-        double xpos = calcX(getTree());
 
-        if (xpos < (getTree().getTreeSize().getLeafSize() / 2.0) || xpos - (getTree().getTreeSize().getLeafSize() * 2.0) > getSCREEN_WIDTH()) {
-            paintTree(getTree(), gc);
-        } else {
-            double ypos = calcY(getTree());
-
-            gc.setFill(getTREE_TRUNK());
-            gc.fillRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2.0), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-            gc.strokeRect(xpos - (getTree().getTreeSize().getTrunkWidth() / 2.0), ypos, getTree().getTreeSize().getTrunkWidth(), getTree().getTreeSize().getTrunkHeight());
-
-            gc.setFill(getTree().getColor());
-
-            double[] doubleX = {
-                    xpos - getTree().getTreeSize().getLeafSize() / 2.0 - getTree().getTreeSize().getTrunkWidth() / 2.0,
-                    xpos + getTree().getTreeSize().getLeafSize() / 2.0 + getTree().getTreeSize().getTrunkWidth() * 1.5,
-                    xpos + getTree().getTreeSize().getTrunkWidth() / 2.0
-            };
-
-            double[] doubleY = {
-                    ypos - getTree().getTreeSize().getTrunkHeight() / 100.0 + 20.0,
-                    ypos - getTree().getTreeSize().getTrunkHeight() / 100.0 + 20.0,
-                    ypos - getTree().getTreeSize().getTrunkHeight() - getTree().getTreeSize().getLeafSize() + 20.0
-            };
-
-            gc.fillPolygon(doubleX, doubleY, 3);
-            gc.strokePolygon(doubleX, doubleY, 3);
-        }
     }
 }
